f6d7c544b6646638e41413d3abd42187682e8a49
hopelesssoulx <hopelesssoulx@gmail.com>
2023-04-28 11:36:49 +0800

    refactor: rewrite with prisma

--- router_handler/guide_page.js
+++ router_handler/guide_page.js
@@ -1,16 +1,33 @@
-const db = require('../db/index')
+// const db = require('../db/index')
 
 
-exports.getGuidePageCarouselImages = (req, res) => {
-    const sql = 'select * from guide_page'
-    db.query(sql, (e, rs) => {
-        if (e) return res.cc(e)
+// exports.getGuidePageCarouselImages = (req, res) => {
+//     const sql = 'select * from guide_page'
+//     db.query(sql, (e, rs) => {
+//         if (e) return res.cc(e)
 
-        return res.send({
-            status: 200,
-            msg: '获取引导页轮播图成功',
-            total: rs.length,
-            data: rs
-        })
+//         return res.send({
+//             status: 200,
+//             msg: '获取引导页轮播图成功',
+//             total: rs.length,
+//             data: rs
+//         })
+//     })
+// }
+
+
+
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+exports.getGuidePageCarouselImages = async (req, res) => {
+    const drafts = await prisma.guide_page.findMany()
+    return res.send({
+        status: 200,
+        msg: '获取引导页轮播图成功',
+        total: drafts.length,
+        data: drafts
     })
 }
